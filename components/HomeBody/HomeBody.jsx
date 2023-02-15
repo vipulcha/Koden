624c7725efcc4aa09ea69715ba9f827d6b75d2a0
Rudransh-03 <75294937+Rudransh-03@users.noreply.github.com>
2023-02-15 15:39:49 +0530

    Add files via upload

---
+++ components/HomeBody/HomeBody.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeBg from '../../images/HomeBg.jpeg'
+import "./HomeBody.css";
+const HomeBody = () => {
+  return (
+    <div className='bg'>
+      <img src={HomeBg} alt="" />
+    </div>
+  )
+}
+
+export default HomeBody
